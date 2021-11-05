22f6999c569e5707a8292cd7f9814a7478712f67
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-05 15:11:46 -0400

    add and validate notes

--- server.js
+++ server.js
@@ -1,31 +1,60 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const path = require("path");
 const { notes } = require("./Develop/db/db.json");
+const PORT = process.env.PORT || 3001;
 
 // unique ids
 const ShortUniqueId = require('short-unique-id'); 
-const uid = new ShortUniqueId();
+const uid = new ShortUniqueId({ length: 5 });
 uid.setDictionary("number");
 
-const PORT = process.env.PORT || 3001;
-
+// middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get("/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "notes.html"))
-// });
+// create a new note to add to the notes array in db.json
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./Develop/db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+};
+
+// makes sure that a string has to be put in for title and text of notes
+function validateNote(note) {
+  if(!note.title || typeof note.title !== "string") {
+    return false;
+  }
+  if(!note.text || typeof note.text !=="string") {
+    return false;
+  }
+  return true;
+}
 
 // reads the db file
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-// post new notes to the db file
+// post new notes to db.json
 app.post("/api/notes", (req, res) => {
-  req.body.id = (uid.seq());
-  res.json(req.body);
+  // unique ids created for each new note
+  req.body.id = uid();
+
+  // if any data in req.body is incorrect, sends 400 error
+  if(!validateNote(req.body)) {
+    res.status(400).send("The note is not formatted correctly.")
+  } else {
+    // add note to db.json and note array
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+  }
 });
 
 app.listen(PORT, () => {
