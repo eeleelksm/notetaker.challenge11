184bb16329d0965cb3a2bcfe6ba2be91a84aa64a
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-05 14:03:06 -0400

    server accepts and reads data

--- server.js
+++ server.js
@@ -5,13 +5,22 @@ const { notes } = require("./Develop/db/db.json");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // app.get("/notes", (req, res) => {
 //   res.sendFile(path.join(__dirname, "notes.html"))
 // });
 
+// reads the db file
 app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+
+// post new notes to the db file
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
