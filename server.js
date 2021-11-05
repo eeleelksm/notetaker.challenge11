499f81491d7c77476dbf3bef2560fcd3e346c56a
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-05 11:43:55 -0400

    testing api server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const { notes } = require("./Develop/db/db.json");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+
+// app.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "notes.html"))
+// });
+
+app.get("/api/notes", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`API server is now on port ${PORT}`);
+});
